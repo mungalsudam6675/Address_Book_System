92927af77d7146a310becdd8fadee647cedaa4fa
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-06 15:41:24 +0530

    Add new contact to address book contact class

--- Contact.java
+++ Contact.java
@@ -1,5 +1,77 @@
 package Address_Book_OOPS;
 
 public class Contact {
+	private String firstName;
+	private String lastName;
+	private String adress;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
 
 }
