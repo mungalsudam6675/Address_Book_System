eb2b9b168cf613c156e371a6daa75fa22d899575
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-06 16:01:39 +0530

    Add new contact in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,6 +4,9 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContacts();
+		addressBook.listPeople();
 
 	}
 
