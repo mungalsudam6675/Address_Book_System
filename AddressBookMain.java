b77f53f8166012a5331a8bedd2e2c8c6ce3b84e0
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-07 01:49:02 +0530

    Add delete and edit method

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,8 +5,10 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 		AddressBook addressBook = new AddressBook();
-		addressBook.addContacts();
+		addressBook.addPerson();
 		addressBook.listPeople();
+		addressBook.editPerson();
+		addressBook.deletePerson();
 
 	}
 
