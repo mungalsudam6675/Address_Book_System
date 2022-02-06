c3d0ff80cffbaf92b4864f7990f7b03f728fe6a5
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-06 16:07:02 +0530

    address book class to implement method

---
+++ AddressBook.java
@@ -0,0 +1,61 @@
+package Address_Book_OOPS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+	List<Contact> people = new ArrayList<Contact>();
+
+	public void addContacts() {
+		Contact contact = new Contact();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Name:");
+		String firstName = sc.nextLine();
+		contact.setFirstName(firstName);
+
+		System.out.println("Enter Last Name:");
+		String lastName = sc.nextLine();
+		contact.setLastName(lastName);
+
+		System.out.println("Enter Adress:");
+		String adress = sc.nextLine();
+		contact.setAdress(adress);
+
+		System.out.println("Enter city:");
+		String city = sc.nextLine();
+		contact.setCity(city);
+
+		System.out.println("Enter state:");
+		String state = sc.nextLine();
+		contact.setState(state);
+
+		System.out.println("Enter zip:");
+		String zip = sc.nextLine();
+		contact.setZip(zip);
+
+		System.out.println("Enter phoneNumber:");
+		String phoneNumber = sc.nextLine();
+		contact.setPhoneNumber(phoneNumber);
+
+		System.out.println("Enter email:");
+		String email = sc.nextLine();
+		contact.setEmail(email);
+
+		people.add(contact);
+		System.out.println("person created sucessfully");
+	}
+
+	public void listPeople() {
+		if (people.isEmpty()) {
+			System.out.println(" address book is empty.");
+			return;
+		}
+		System.out.println("Address Book:");
+		for (Contact element : people) {
+			System.out.print(element.getFirstName());
+		}
+
+	}
+}
