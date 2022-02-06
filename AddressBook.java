2d57e19675996aeba94b7a9daa8a6f01365ae9bd
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-07 02:12:59 +0530

    Add delete and edit method

--- AddressBook.java
+++ AddressBook.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class AddressBook {
 	List<Contact> people = new ArrayList<Contact>();
+	Scanner sc = new Scanner(System.in);
 
-	public void addContacts() {
+	public void addPerson() {
 		Contact contact = new Contact();
 
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first Name:");
 		String firstName = sc.nextLine();
 		contact.setFirstName(firstName);
@@ -60,18 +60,59 @@ public class AddressBook {
 	}
 
 	public void deletePerson() {
-		Scanner sc = new Scanner(System.in);
-         System.out.println("Enter the first name you want to remove.");
-         String firstName = sc.nextLine();
-         for(int i=0;i<people.size();i++) {
-        	 if(people.get(i).getFirstName().equals(firstName)) {
-        		 people.remove(people.get(i));
-        		 System.out.println("person removed sucessfully");
-        	 }
-        	 else {
-        		 System.out.println("person not found");
-        	 }
-        	 
-         	}	
+		System.out.println("Enter the first name you want to remove.");
+		String firstName = sc.nextLine();
+		for (int i = 0; i < people.size(); i++) {
+			if (people.get(i).getFirstName().equals(firstName)) {
+				people.remove(people.get(i));
+				System.out.println("person removed sucessfully");
+			} else {
+				System.out.println("person not found");
+			}
+
+		}
+	}
+
+	public void editPerson() {
+		System.out.println("Enter the first name you want to edit.");
+
+		String firstName = sc.nextLine();
+
+		for (int i = 0; i < people.size(); i++) {
+			if (people.get(i).getFirstName().equals(firstName)) {
+
+				people.get(i).setFirstName(firstName);
+
+				System.out.println("Enter Last Name:");
+				String lastName = sc.nextLine();
+				people.get(i).setLastName(lastName);
+
+				System.out.println("Enter Adress:");
+				String address = sc.nextLine();
+				people.get(i).setAdress(address);
+
+				System.out.println("Enter state:");
+				String state = sc.nextLine();
+				people.get(i).setState(state);
+
+				System.out.println("Enter zip:");
+				String zip = sc.nextLine();
+				people.get(i).setZip(zip);
+
+				System.out.println("Enter phoneNumber:");
+				String phoneNumber = sc.nextLine();
+				people.get(i).setPhoneNumber(phoneNumber);
+
+				System.out.println("Enter email:");
+				String email = sc.nextLine();
+				people.get(i).setEmail(email);
+
+				System.out.println("person edited sucessfully");
+			} else {
+				System.out.println("person not found");
+			}
+
+		}
+
 	}
 }
